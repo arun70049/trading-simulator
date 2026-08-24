bb0c38021a2c4a4923730fbf279dba668665074e
Arun Kumar <arunkr.am7@gmail.com>
2026-08-24 05:58:43 +0530

    Add INITIAL_STOCKS array with stock information
    Added initial stock data for various companies.

---
+++ data.js
@@ -0,0 +1,38 @@
+const INITIAL_STOCKS = [
+  {
+    symbol: "RELIANCE",
+    name: "Reliance Industries",
+    price: 2450,
+    previousClose: 2450
+  },
+  {
+    symbol: "HDFCBANK",
+    name: "HDFC Bank",
+    price: 1750,
+    previousClose: 1750
+  },
+  {
+    symbol: "INFY",
+    name: "Infosys",
+    price: 1880,
+    previousClose: 1880
+  },
+  {
+    symbol: "TATAMOTORS",
+    name: "Tata Motors",
+    price: 720,
+    previousClose: 720
+  },
+  {
+    symbol: "ICICIBANK",
+    name: "ICICI Bank",
+    price: 1320,
+    previousClose: 1320
+  },
+  {
+    symbol: "ITC",
+    name: "ITC",
+    price: 430,
+    previousClose: 430
+  }
+];
